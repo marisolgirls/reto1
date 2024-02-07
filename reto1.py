30017cfca9652998030927e0b66d3bb9e4925d3d
frishalin@gmail.com <marisolgirls33@gmail.com>
2024-02-07 17:52:48 -0600

    reto 1

---
+++ reto1.py
@@ -0,0 +1,8 @@
+print("Primer reto")
+
+nombre = input("Escribe tu nombre: ")
+apellidos = input("Escribe tus apellidos: ");
+telefono = input("Escribe tu número telefónico")
+email = input("Ingresa tu correo electrónico: ")
+
+print("Hola", nombre, apellidos + ", en breve recibirás un correo a " + email + ".")
